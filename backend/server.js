7addc8848f66caaa02b90b75440ca6c858534c87
Souad <souadshhy@gmail.com>
2026-08-30 19:06:35 +0300

    fixed express wildcard syntax

--- backend/server.js
+++ backend/server.js
@@ -55,7 +55,7 @@ app.post("/api/content", async (req, res) => {
 
 app.use(express.static(path.join(__dirname, "dist")));
 
-app.get("*", (req, res) => {
+app.get("/(.*)", (req, res) => {
   res.sendFile(path.join(__dirname, "dist", "index.html"));
 });
 
