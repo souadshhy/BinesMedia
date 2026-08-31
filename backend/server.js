927f1b21246fa0f7a8dd3757efc2a4228d78e3d5
Souad <souadshhy@gmail.com>
2026-08-31 14:13:50 +0300

    Fixed wildcard syntax

--- backend/server.js
+++ backend/server.js
@@ -182,7 +182,7 @@ app.post("/api/content", verifyToken, async (req, res) => {
 
 // Serve React Frontend
 app.use(express.static(path.join(__dirname, "dist")));
-app.get("*", (req, res) => {
+app.get("/{*splat}", (req, res) => {
   res.sendFile(path.join(__dirname, "dist", "index.html"));
 });
 
